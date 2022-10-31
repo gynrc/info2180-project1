941d9fa30ff6a9d26edec84905e73c877acbbe53
Condoleezza Gaynor <condeleezagaynor@gmail.com>
2022-10-30 23:43:15 -0500

    updated script/stylesheet

--- scripts.js
+++ scripts.js
@@ -1,24 +1,18 @@
 /* Add your JavaScript to this file */
 window.onload = function () {
     
-    const subBtn = document.querySelector('.btn');
-    var mailformat = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
-    var message = document.querySelector('.message');
-    var email = document.getElementById('email').value;
-    var form = document.querySelectorAll('.form-group');
-    subBtn.addEventListener('submit', validation);
+    let message = document.querySelector('.message');
+    let email = document.getElementById('email');
     
-    function validation() {
-        if (email.test(mailformat)) 
+    addEventListener('submit', (validation) => {
+        validation.preventDefault();
+        if (!email.value == '') 
         {
-            message.innerHTML = 'Thank you! Your email address ' + email + ' has been added to our mailing list!';
-            //message.style.visibility = "visible";
-            message.style.color = '#00ff00';
+            message.innerHTML = 'Thank you! Your email address ' + email.value + ' has been added to our mailing list!';
+            message.style.color = '#377b93';
         } else {
             message.innerHTML = 'Please enter a valid email address.';
-            message.style.color = '##ff0000';
+            message.style.color = '#e0493b';
         }
-        console.log("button clicked!");
-      }
-
+    });
 }
